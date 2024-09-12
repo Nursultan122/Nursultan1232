be11aeccc38e7310b737fa5393d8a5114ea50861
Nursultan122 <naltymysov986@gmail.com>
2024-09-12 16:30:37 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>41654654</p>
+    </>
+  )
+}
+Function() {
+  <p>
+    const str1 
+  </p>
+  const str2 = 'text2';
+}
+
+export default App
